a75ed0eedbd98b1e9c5c10c3a20326340eade404
dincuy <dincuy23@gmail.com>
2022-07-19 20:43:55 +0700

    deploy ke heroku

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&0@p-%h#52vvt+eys47me883vdifcle^qww0%gt2d4ovso8n3l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
--- readme.md
+++ readme.md
@@ -1,3 +1,8 @@
 **aplikasi hanya berisi:**
 - 1 model post
 - test untuk model post
+
+**Heroku Config**
+1. install gunicorn==19.9.0
+2. add a Procfile -> web: gunicorn config.wsgi --log-file -
+3. update ALLOWED_HOSTS -> .herokuapp.com, localhost, 127.0.0.1
